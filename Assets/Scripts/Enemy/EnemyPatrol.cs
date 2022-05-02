8e36aaf44ef3b08fc69ef672f46081fafe409e75
Jaime Abad <jabadg99@gmail.com>
2022-05-02 20:14:50 +0200

    Added patrol

--- Assets/Scripts/Enemy/EnemyPatrol.cs
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -5,8 +5,8 @@ using UnityEngine.AI;
 
 public class EnemyPatrol : MonoBehaviour
 {
-    [SerializeField] private Transform[] _referenceToMoveB;
-    [SerializeField] private GameObject[] _referencesB;
+    [SerializeField] private Transform[] _referenceToMoveA, _referenceToMoveB, _referenceToMoveC;
+    [SerializeField] private GameObject[] _referencesA, _referencesB, _referencesC;
     private int _cont;
     private bool _referencesComplete;
 
@@ -19,7 +19,7 @@ public class EnemyPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, _referenceToMoveB[4].position, 0.001f);
     }
 
     private void InitParameters()
@@ -27,6 +27,21 @@ public class EnemyPatrol : MonoBehaviour
         _cont = 0;
         _referencesB = new GameObject[5]; //Put exact number
         _referenceToMoveB = new Transform[5]; //Put exact number
+        /*
+        #region Get references pavilion a
+        do
+        {
+            _referencesComplete = false;
+            _referencesA[_cont] = GameObject.Find("ReferenceA" + _cont);
+            _referenceToMoveA[_cont] = _referencesB[_cont].GetComponent<Transform>();
+            _cont++;
+            if (GameObject.Find("ReferenceA" + _cont) == null)
+            {
+                _referencesComplete = true;
+            }
+        } while (_referencesComplete == false);
+        #endregion
+        */
         #region Get references pavilion b
         do
         {
@@ -40,8 +55,22 @@ public class EnemyPatrol : MonoBehaviour
             }
         } while (_referencesComplete == false);
         #endregion
-        GetComponent<NavMeshAgent>().SetDestination(_referenceToMoveB[4].position);
-        GetComponent<NavMeshAgent>().speed = 3;
+        /*
+        #region Get references pavilion c
+        do
+        {
+            _referencesComplete = false;
+            _referencesC[_cont] = GameObject.Find("ReferenceC" + _cont);
+            _referenceToMoveC[_cont] = _referencesB[_cont].GetComponent<Transform>();
+            _cont++;
+            if (GameObject.Find("ReferenceC" + _cont) == null)
+            {
+                _referencesComplete = true;
+            }
+        } while (_referencesComplete == false);
+        #endregion
+        */
+        GetComponent<Animator>().SetFloat("Speed", 2);
 
     }
 }
